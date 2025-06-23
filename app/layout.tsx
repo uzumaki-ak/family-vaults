eff947cb2985c8197d3998c244d04f9b05356953
uzumaki-ak <anikydv38@gmail.com>
2025-06-23 19:15:29 +0530

    error fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,24 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'family-vault',
-  description: 'google photos alternative',
-authors: [{ name: "anikesh" }],
-icons : "./logo.png"
-}
+  title: "Google photos like platform",
+  description:
+    "Preserve and celebrate your familys most cherished moments with Legacy — a secure, shared digital vault for photos, videos, and messages. Upload, organize, and relive your memories together with powerful features like timelines, audio/video support, legacy notes, and voting-based deletion.",
+
+  keywords:
+    "family memory vault, digital memory box, legacy photo storage, family photo archive, audio memories, legacy notes, shared family albums, media timeline app, private family gallery",
+  authors: { name: "Anikesh" },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
